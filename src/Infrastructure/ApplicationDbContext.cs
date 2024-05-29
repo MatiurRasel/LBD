c0148307635771baedcfd8c4de1b6d453a9c0aed
MatiurRasel <matiurrasel1002@gmail.com>
2024-05-29 15:17:18 +0600

    --

---
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,98 @@
+﻿using Application.Abstractions.Clock;
+using Application.Exceptions;
+using Domain.Entities.Abstractions;
+using Infrastructure.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    private static readonly JsonSerializerSettings JsonSerializerSettings = new()
+    {
+        TypeNameHandling = TypeNameHandling.All
+    };
+
+    //private readonly IPublisher _publisher;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public ApplicationDbContext(DbContextOptions options,
+        //IPublisher publisher,
+        IDateTimeProvider dateTimeProvider) : base(options)
+    {
+        //_publisher = publisher;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            //First process the domain events and adding them to ChangeTracker as Outbox Messages,
+            //then persisting everything in the database in a single transaction "atomic operation" 
+            AddDomainEventsAsOutboxMessages();
+
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception ocurred.", ex);
+        }
+
+    }
+
+    //Publishing an event can fail and is not in a transaction. Changing to Outbox pattern
+    //private async Task PublishDomainEventsAsync()
+    //{
+    //    /*
+    //     Using the ChangeTracker, we grab the entries which implement the Entity class. 
+    //     Then we grab all the domain events and publish one by one.         
+    //     */
+    //    var domainEvents = ChangeTracker
+    //        .Entries<IEntity>()
+    //        .Select(entry => entry.Entity)
+    //        .SelectMany(entity =>
+    //        {
+    //            var domainEvents = entity.GetDomainEvents();
+
+    //            entity.ClearDomainEvents();
+
+    //            return domainEvents;
+    //        }).ToList();
+
+    //    foreach (var domainEvent in domainEvents) await _publisher.Publish(domainEvent);
+    //}
+
+    private void AddDomainEventsAsOutboxMessages()
+    {
+        var outboxMessages = ChangeTracker
+            .Entries<IEntity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .Select(domainEvent => new OutboxMessage(
+                Guid.NewGuid(),
+                _dateTimeProvider.UtcNow,
+                domainEvent.GetType().Name,
+                JsonConvert.SerializeObject(domainEvent, JsonSerializerSettings)))
+            .ToList();
+
+        AddRange(outboxMessages);
+    }
+}
